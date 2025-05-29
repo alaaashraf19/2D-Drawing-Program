658dcf22bf7923d7111efa798365e2f619339431
Salma884 <147331892+Salma884@users.noreply.github.com>
2025-05-29 22:09:40 +0300

    Update utils.h

--- Graphics Project- V1/utils.h
+++ Graphics Project- V1/utils.h
@@ -7,13 +7,3 @@ struct Point {
     int x, y;
     Point(int x = 0, int y = 0) : x(x), y(y) {}
 };
-
-Point operator*(double a, const Point& p) {
-    return Point(a * p.x, a * p.y);
-}
-
-
-Point operator+(const Point& p, const Point& p2) {
-    return Point(p2.x + p.x, p2.y + p.y);
-}
-
